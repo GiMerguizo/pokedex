2aa3ecd82f918dc9c3582f85cdbc3922559cb8c3
GiMerguizo <giovana.pontes5654@hotmail.com>
2025-01-11 19:47:30 -0300

    Update front page details

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,18 +11,20 @@
     
     function convertPokemonToLi(pokemon) {
         return `
-            <li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
-    
-                <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
-    
-                    <img src="${pokemon.photo}" alt="${pokemon.name}">
-                </div>
-            </li>
+            <a href="./details.html" target="_blank">
+                <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+        
+                    <div class="detail">
+                        <ol class="types">
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                        </ol>
+        
+                        <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    </div>
+                </li>
+            </a>
         `
     }
     
